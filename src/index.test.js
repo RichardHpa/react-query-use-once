5608cc6da41a8342cf6438152aac2f52b81b87f6
RichardHpa <richard.hpa@hotmail.com>
2021-10-05 21:02:17 +1300

    test(testing): added MSW and tests for hook

--- src/index.test.js
+++ src/index.test.js
@@ -1,9 +1,39 @@
+import { jest } from '@jest/globals'
 import { renderHook } from '@testing-library/react-hooks'
-import useTemplate from '.'
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
+import { useQueryOnce } from '.'
 
-describe('npm-package-template ', () => {
-	it('should return starter text', () => {
-		const { result } = renderHook(() => useTemplate())
-		expect(result.current).toBe('You are using the default template.')
+const fetchFromApi = async (url) => {
+	const res = await fetch(url)
+	const payload = await res.json()
+	return payload
+}
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: false,
+		},
+	},
+})
+const wrapper = ({ children }) => (
+	<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+
+describe('useQueryOnce', () => {
+	it('should should call successfully', async () => {
+		const { result, waitFor } = renderHook(
+			() => useQueryOnce('key', () => fetchFromApi('/getPosts')),
+			{
+				wrapper,
+			}
+		)
+		jest.setTimeout(10000)
+		await waitFor(() => result.current.isSuccess)
+		expect(result.current.data).toEqual('Hello World')
 	})
 })
+
+// test("should fetch", async () => {
+// 	await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1), { timeout: 8000 });
+// }, 10000);
