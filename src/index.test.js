c633402272e5cc3dd5b6ce1eeb7fa1495cd86ee9
Richard <richard.m.hpa@gmail.com>
2022-08-25 09:24:36 +1200

    test(index.test): edited tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,39 +1,36 @@
-import { jest } from '@jest/globals'
-import { renderHook } from '@testing-library/react-hooks'
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query'
-import useQueryOnce from '.'
+import { jest } from '@jest/globals';
+import { renderHook } from '@testing-library/react-hooks';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import useQueryOnce from '.';
 
-const fetchFromApi = async (url) => {
-	const res = await fetch(url)
-	const payload = await res.json()
-	return payload
-}
+const fetchFromApi = async url => {
+  const res = await fetch(url);
+  const payload = await res.json();
+  return payload;
+};
 
 const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			retry: false,
-		},
-	},
-})
+  defaultOptions: {
+    queries: {
+      retry: false,
+    },
+  },
+});
 const wrapper = ({ children }) => (
-	<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
-)
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
 
 describe('useQueryOnce', () => {
-	it('should should call successfully', async () => {
-		const { result, waitFor } = renderHook(
-			() => useQueryOnce('key', () => fetchFromApi('/getPosts')),
-			{
-				wrapper,
-			}
-		)
-		jest.setTimeout(10000)
-		await waitFor(() => result.current.isSuccess)
-		expect(result.current.data).toEqual('Hello World')
-	})
-})
-
-// test("should fetch", async () => {
-// 	await waitFor(() => expect(fetch).toHaveBeenCalledTimes(1), { timeout: 8000 });
-// }, 10000);
+  it('should should call successfully', async () => {
+    const { result, waitFor } = renderHook(
+      () => useQueryOnce('key', () => fetchFromApi('/getPosts')),
+      {
+        wrapper,
+      }
+    );
+    jest.setTimeout(10000);
+    await waitFor(() => result.current.isSuccess);
+    expect(result.current.data).toEqual('Hello World');
+    expect(result.current.isSuccess).toBe(true);
+  });
+});
